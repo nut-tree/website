a3a6ee5dfab9b70eaaddc85ddb66e30bffdbaafc
Simon Hofmann <s1hofmann@users.noreply.github.com>
2021-06-12 23:47:59 +0200

    Update button text  on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            nut.js Quickstart ⏱️
           </Link>
         </div>
       </div>
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="nut.js - node desktop automation">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
