b457000dba60ea184bba25a223a1956301c0ced2
Simon Hofmann <s1hofmann@users.noreply.github.com>
2021-06-12 23:44:21 +0200

    Update landing page content

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,61 +4,58 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+title: 'Easy to Install',
+Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+description: (
+  <>
+    nut.js was developed with a focus on ease of installation. Everything is at your fingertips, just an <code>npm install</code> away!
+  </>
+),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+title: 'Easy to Use',
+Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+description: (
+  <>
+    An API which is loved by users for it's ease of use and expressiveness let's you automate in a breeze!
+  </>
+),
   },
   {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+title: 'Three platforms, single codebase',
+Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+description: (
+  <>
+    nut.js is focused on cross-platform compatibility and equally supports macOS, Linux and Windows.
+  </>
+),
   },
 ];
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
+<div className={clsx('col col--4')}>
+  <div className="text--center">
+    <Svg className={styles.featureSvg} alt={title} />
+  </div>
+  <div className="text--center padding-horiz--md">
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </div>
+</div>
   );
 }
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
+<section className={styles.features}>
+  <div className="container">
+    <div className="row">
+      {FeatureList.map((props, idx) => (
+        <Feature key={idx} {...props} />
+      ))}
+    </div>
+  </div>
+</section>
   );
 }
