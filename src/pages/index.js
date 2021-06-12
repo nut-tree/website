0e3b97e525b164364f9c172a7bfdbd73439788e2
Simon Hofmann <s1hofmann@users.noreply.github.com>
2021-06-12 23:52:11 +0200

    Changed icon and wording

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            nut.js Quickstart ⏱️
+            Quickstart 🚀
           </Link>
         </div>
       </div>
