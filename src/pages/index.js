e075266fa5bb2bf7ebec190349deca9736c71c21
Simon Hofmann <mail@simon-hofmann.org>
2021-12-14 10:18:54 +0100

    First overhaul with added terminal animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,14 @@ import Link from '@docusaurus/Link';
 import Head from '@docusaurus/Head';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import NutJsLogo from '@site/static/img/logo.svg';
 
 import HomepageFeatures from '../components/HomepageFeatures';
+import Terminal from "../components/Terminal";
 
 function HomepageHeader() {
-    const {siteConfig} = useDocusaurusContext();
     return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <header className={clsx('hero hero--dark', styles.heroBanner)}>
             <div className="container">
-                <NutJsLogo style={{width: "20%", height: "20%"}}/>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
@@ -35,10 +32,12 @@ export default function Home() {
             title={`Hello from ${siteConfig.title}`}
             description="nut.js - node desktop automation">
             <Head>
-            <script defer data-domain="nutjs.dev" src="https://plausible.io/js/plausible.js"></script>
+                <script defer data-domain="nutjs.dev" src="https://plausible.io/js/plausible.js"/>
+                <title>{`Hello from ${siteConfig.title}`}</title>
             </Head>
-            <HomepageHeader/>
+            <Terminal/>
             <main>
+                <HomepageHeader/>
                 <HomepageFeatures/>
             </main>
         </Layout>
