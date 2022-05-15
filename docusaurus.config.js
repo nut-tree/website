0600fd1806f50ed42e14d7a6434e02a6fef2992c
a-marquez <albert.marquez.2012@gmail.com>
2022-05-15 10:58:01 -0400

    Fixed editUrl location

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,13 +82,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/nut-tree/website/edit/main/website/',
+            'https://github.com/nut-tree/website/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/nut-tree/website/edit/main/website/blog/',
+            'https://github.com/nut-tree/website/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
